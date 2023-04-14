6e6b58901828d217b57c8eea310fd24d65ee3d3e
Jimike110 <oladoyemike@gmail.com>
2023-04-14 09:25:24 +0100

    Edit: removed latest config start button edits

--- script.js
+++ script.js
@@ -67,11 +67,9 @@ document.getElementById("config-next").addEventListener("click", () => {
           quizArray.push(quizObject);
         }
         // Hide the config page and show the quiz page
-        configNext.innerHTML = "...";
         categoryDiv.style.display = "none";
         displayContainer.style.display = "block";
         initial();
-        configNext.setAttribute("disabled");
       });
   }
 });
